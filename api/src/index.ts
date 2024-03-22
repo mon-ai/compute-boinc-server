0370c565a531fd155e76613737ad8fe351c64f14
疒奀 <46226846+mlegls@users.noreply.github.com>
2024-03-22 16:38:37 +0800

    new endpoint for boinc2docker command

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,9 +1,28 @@
 import { Elysia } from "elysia";
+import { cors } from "@elysiajs/cors";
+import { $ } from "bun";
 
 const app = new Elysia()
-  .get("/", () => ({
-    ok: "true"
+  .use(
+    cors({
+      origin: "co.mmon.co",
+    })
+  )
+  .get("/healthcheck", () => ({
+    ok: true,
   }))
+  .post("/boinc2docker-app", async (req) => {
+    if (typeof req.body !== "string") return req.error(400, "missing body");
+
+    const { cmd } = JSON.parse(req.body);
+    if (!cmd) return req.error(400, "missing command");
+
+    const result = await $`echo ${cmd} > out.txt`;
+    if (result.stderr.length)
+      return req.error(500, { error: result.stderr.join("\n") });
+
+    return { ok: true, result: result.stdout.join("\n") };
+  })
   .listen(8000);
 
 console.log(
